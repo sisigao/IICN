d1301ba561ac27420f89f767e2bbaab10de11bbc
sisigao <73464285+sisigao@users.noreply.github.com>
2025-12-24 19:14:02 +0800

    Update IICN-5.py

--- Models/Page/IICN-5.py
+++ Models/Page/IICN-5.py
@@ -1,5 +1,5 @@
 import torch
-import torch.nn as nn
+import torch.nn as nn 
 import torch.nn.functional as F
 from collections import OrderedDict
 from Models.utils.layer import Attention, MultiLayerPerceptron
